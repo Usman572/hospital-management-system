3aca4a2c8108c01cdc7431a65f5e680cb3983587
usman Ali <usmansky72472@gmail.com>
2026-07-31 03:46:35 +0500

    HMS-013 implement API versioning

--- apps/api/src/modules/patient/patient.controller.ts
+++ apps/api/src/modules/patient/patient.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Version } from '@nestjs/common';
 import { CreatePatientDto } from './dto/create-patient.dto';
 
 @Controller('patients')
 export class PatientController {
+  @Version('1')
   @Post()
   create(@Body() dto: CreatePatientDto) {
     return {
