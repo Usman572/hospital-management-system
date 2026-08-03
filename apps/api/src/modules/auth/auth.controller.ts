cb96c81fac90db922369393efabc32adf78dcdc2
usman Ali <usmansky72472@gmail.com>
2026-08-04 02:47:27 +0500

    feat(HMS-021): implement current user decorator and authorization

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,14 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
-
-@ApiTags('Auth')
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { CurrentUser } from './decorators/current-user.decorator';
+import type { CurrentUserPayload } from './interfaces/current-user.interface';
 @Controller('auth')
 export class AuthController {
-
   constructor(
     private readonly authService: AuthService,
   ) {}
@@ -20,11 +26,18 @@ export class AuthController {
     return this.authService.register(dto);
   }
 
-
   @Post('login')
   login(
     @Body() dto: LoginDto,
   ) {
-    return this.authService.login(dto)
+    return this.authService.login(dto);
   }
+
+  @UseGuards(JwtAuthGuard)
+@Get('me')
+getProfile(
+  @CurrentUser() user: CurrentUserPayload,
+) {
+  return user;
+}
 }
